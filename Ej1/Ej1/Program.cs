1c7130e9a44ff5c72429604b7dbf51039d92c477
Íker Belles <93086193+Ikebar52@users.noreply.github.com>
2023-02-21 18:44:00 +0100

    Ej1 acabado

---
+++ Ej1/Ej1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+			Console.WriteLine("Bienvenido a la papelería, ¿Cuántos paquetes de 500 hojas quieres?");
+			int paquete = Convert.ToInt32(Console.ReadLine());
+			float precio = 1;
+			if (paquete < 200) precio = 4.5f;
+			else if (200 <= paquete && paquete <= 500) precio = 3.75f;
+			else if (paquete > 500) precio = 3.25f;
+			else if(paquete <= 0) Console.WriteLine("¿Has venido a comprar o no?");
+			float precio_final = paquete * precio;
+			Console.WriteLine("El precio final es de " + precio_final + " euros.");
+			Console.ReadKey();
+
+		}
+	}
+}
--- Ej1/Ej1/Properties/AssemblyInfo.cs
+++ Ej1/Ej1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Pract1")]
+[assembly: AssemblyTitle("Ej1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Universitat Jaume I")]
-[assembly: AssemblyProduct("Pract1")]
+[assembly: AssemblyProduct("Ej1")]
 [assembly: AssemblyCopyright("Copyright © Universitat Jaume I 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("5ce1784b-c440-46f3-80bd-a5baad080cdf")]
+[assembly: Guid("68883afa-0bd1-4807-9da6-6edfac5d5b32")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
